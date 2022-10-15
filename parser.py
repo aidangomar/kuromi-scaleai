5756a01d3afb7876091da7c6d36480f2594585e1
susuwatarii <aidangomar@gmail.com>
2022-10-15 14:11:35 -0400

    figuring out webdev

--- parser.py
+++ parser.py
@@ -2,5 +2,6 @@
 from dbfread import DBF
 f = open('mariupol.csv', 'w')
 for record in DBF('dataset/mariupol.dbf'):
-    f.write(record['bounds'] + "," + record['status'] + '\n')
+    f.write('[' + record['bounds'] + ']' + "," + record['status'] + '\n')
 
+f.close()
